32e28a5cf6521b4b42f161f1bfe9d94ac4b38f1f
Ahmed Hussein <ahmedallam5852@gmail.com>
2025-11-29 15:08:24 +0200

    update gat alladmin flights

--- controllers/flights/getAllFlights.js
+++ controllers/flights/getAllFlights.js
@@ -2,14 +2,20 @@ const flights = require("../../model/flights");
 
 const get_all_flights = async (req, res) => {
   try {
-    const limit = parseInt(req.query.limit) || 5;
-    const page = parseInt(req.query.page) || 1;
-    const skip = (page - 1) * limit;
-    const allFlights = await flights
-      .find({}, { __v: 0 })
-      .skip(skip)
-      .limit(limit);
+    const isAdmin = String(req.query.all).toLowerCase() === "true";
+    console.log(isAdmin,req.query.all);
 
+    let allFlights;
+
+    if (isAdmin) {
+      allFlights = await flights.find({}, { __v: 0 });
+    } else {
+      const limit = parseInt(req.query.limit) || 5;
+      const page = parseInt(req.query.page) || 1;
+      const skip = (page - 1) * limit;
+
+      allFlights = await flights.find({}, { __v: 0 }).skip(skip).limit(limit);
+    }
     res.status(200).json({
       message: "success",
       data: allFlights,
